67d7e94afa7b8baae2be5a1ba0c1b30827b359cd
Jeremy Berman <jerber@sas.upenn.edu>
2020-05-24 22:06:34 -0400

    added more to readme

--- src/tests/all/from_readme.py
+++ src/tests/all/from_readme.py
@@ -1,18 +1,12 @@
-# MagicDB
-The easiest way to use store data.
+from tests import config
 
-## Instalation
-```
-pip install magicdb
-```
-
-## Example
-```python
 from magicdb.Models import MagicModel
 
+
 class Salesman(MagicModel):
-    name: str = None
-    company: str = None
+	name: str = None
+	company: str = None
+
 
 s = Salesman()
 s.name = 'Jim'
@@ -20,15 +14,10 @@ s.save()
 
 # Get Salesman
 s = Salesman.collection.get(s.id)
-print(s.name) # Jim
-```
+print(s.name, s.id)  # Jim
 
-## Fields
-Use any type [mypy](http://mypy-lang.org/) will accept!
-
-#### Fields Example
-```python
 from datetime import datetime
+
 
 class Manager(MagicModel):
 	name: str
@@ -36,23 +25,22 @@ class Manager(MagicModel):
 	company: str = 'Dunder Mifflin'
 	startedWorkingAt: datetime = None
 
+
 m = Manager(name='Michael Scott')  # you can pass in fields or set them later
 m.age = 45
 m.save()  # Success! New doc in collection "manager" as: { name: Michael Scott, age: 45, company: Dunder Mifflin }
 
 m = Manager()
 m.name = 'Dwight Schrute'
-print(m.save())  # Exception since age is required but not given
 
-```
 
-You can also add other Objects as a field.
+# print(m.save())  # Exception since age is required but not given
 
-### NestedModel Example
-```python
+
 class Dog(MagicModel):
 	age: int
 	owner: Manager
+
 
 dog = Dog()
 dog.age = 3
@@ -60,15 +48,7 @@ dog.owner = Manager(name='Robert California', age=59)
 dog.save()
 print(dog)
 
-```
 
-
-## Collections
-The collection name for a class defaults to the class' name in lowercase. To set the collection name, use the `Meta` class.
-
-### Meta Example
-
-```python
 class Student(MagicModel):
 	name: str = None
 	school: str = 'UPenn'
@@ -80,38 +60,23 @@ class Student(MagicModel):
 s = Student(name='Amy Gutman')
 s.save()  # creates a new document in the "students" collection
 print(s)  # name='Amy Gutman' school='UPenn'
-```
 
-You can also inheret classes.
 
-### Inheritance Example
-```python
 class ExchangeStudent(Student):
 	originalCountry: str
 
 	class Meta:
 		collection_name = 'exchangeStudents'
 
-e = ExchangeStudent(originalCountry='UK')
-print(e.school)  # UPenn
-e.save()
-print(e)  # name=None school='UPenn' originalCountry='UK'
-```
-
-## Queries
-You can make queries with the same syntax you would using the Python firebase-admin SDK. But FireORM returns the objects.
-
-### Queries Example
-```python
 
 e = ExchangeStudent(originalCountry='UK')
 print(e.school)  # UPenn
 e.save()
 print(e)  # name=None school='UPenn' originalCountry='UK'
+
 
 managers = Manager.collection.where('name', '==', 'Michael Scott').limit(1).stream()
 print(managers) # [Manager(name='Michael Scott', age=45, company='Dunder Mifflin', startedWorkingAt=None)]
 print(managers[0].id)
 manager = Manager.collection.get('0mIWZ8FfgQzBanCllqsV')
 print(manager) # name='Michael Scott' age=45 company='Dunder Mifflin' startedWorkingAt=None
-```
